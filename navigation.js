6c06fef5c17a44259ee3f18d234b360c74b30f2b
zhenya9876 <zhenya9809@mail.ru>
2022-03-25 00:19:52 +0500

    Wrapped elementNodes array into object with 'ElementNodes' constructor. Maybe overcomplicating the system

--- navigation.js
+++ navigation.js
@@ -1,5 +1,5 @@
 var elements = [];
-var elementNodes = [];
+var elementNodes;
 var currentNode;
 window.onload = init;
 function init () {
@@ -9,18 +9,34 @@ function init () {
     elements[3] = document.getElementById('elem4');
     elements[4] = document.getElementById('elem-cont1');
 
-    elementNodes.push(new ElementNode(0, elements[0].id, -1, 4, -1, -1));
+    elementNodes = new ElementNodes();
+    elementNodes.addNode(elements[0].id, -1, 4, -1, -1);
 
-    elementNodes.push(new ElementNode(1, elements[1].id, -1, 2, 3, 0));
-    elementNodes.push(new ElementNode(2, elements[2].id, -1, -1, -1, 1));
-    elementNodes.push(new ElementNode(3, elements[3].id, 1, -1, -1, 0));
-    elementNodes.push(new ElementNode(4, elements[4].id, -1, -1, -1, 0));
+    elementNodes.addNode(elements[1].id, -1, 2, 3, 0);
+    elementNodes.addNode(elements[2].id, -1, -1, -1, 1);
+    elementNodes.addNode(elements[3].id, 1, -1, -1, 0);
+    elementNodes.addNode(elements[4].id, -1, -1, -1, 0);
 
-    elementNodes[4].addChild(elementNodes[1].id);
-    elementNodes[4].addChild(elementNodes[2].id);
-    elementNodes[4].addChild(elementNodes[3].id);
+    elementNodes.nodes[4].addChild(elementNodes.nodes[1].id);
+    elementNodes.nodes[4].addChild(elementNodes.nodes[2].id);
+    elementNodes.nodes[4].addChild(elementNodes.nodes[3].id);
 }
+function ElementNodes(){
+    this.lastId = 0;
+    this.nodes = [];
+    this.addNode = function (name, nodeTop, nodeRight, nodeBottom, nodeLeft) {
+        this.nodes.push(new ElementNode(this.lastId, name, nodeTop, nodeRight, nodeBottom, nodeLeft));
+        this.lastId++;
+    };
+    this.removeNode = function (nodeId) {
+        this.nodes.splice(this.nodes.indexOf(nodeId),1);
+    };
+    this.getNodeByName = function (name) {
+        return this.nodes.filter(function (name) {
 
+        });
+    }
+}
 // Element Node Object Constructor
 function ElementNode(id, name, nodeTop, nodeRight, nodeBottom, nodeLeft) {
     this.id = id;
@@ -35,84 +51,58 @@ function ElementNode(id, name, nodeTop, nodeRight, nodeBottom, nodeLeft) {
 
     this.addChild = function (childNodeId) {
         this.childrenNodesIds.push(childNodeId)
-        elementNodes[childNodeId].parentId = this.id;
+        elementNodes.nodes[childNodeId].parentId = this.id;
     }
-    this.findNextNodeId = function (direction) {
-        var resultId;
-        switch (direction){
-            case 0: //top
-                resultId = this.nodeTop;
-                break;
-            case 1: //right
-                resultId = this.nodeRight;
-                break;
-            case 2: //bottom
-                resultId = this.nodeBottom;
-                break;
-            case 3: //left
-                resultId = this.nodeLeft;
-                break;
-        }
-        if(resultId !== -1){
-            var tempNode = elementNodes[resultId];
-            if(tempNode.childrenNodesIds.length === 0) { // if target is also a leaf
-                return resultId;
-            } else {    //if target is not a leaf - go down to a leaf
-                while(tempNode.childrenNodesIds.length !== 0) {
-                    if (tempNode.lastSelectedChildId !== -1) {
-                        tempNode = elementNodes[tempNode.lastSelectedChildId];
-                    }
-                    else tempNode = elementNodes[tempNode.childrenNodesIds[0]];
-                }
-                return tempNode.id;
-            }
-        }
-        return -1;
+    this.removeChild = function (childNodeId){
+        this.childrenNodesIds.splice(
+            this.childrenNodesIds.indexOf(elementNodes.nodes[childNodeId]), 1);
+        if (this.lastSelectedChildId === childNodeId) this.lastSelectedChildId = -1;
     }
     this.goToNode = function (goToId){
         if(goToId !== -1) {
-            var tempNode = elementNodes[this.id];
+            var tempNode = elementNodes.nodes[this.id];
+            // here we go up a branch to record the last selected children of nodes we are leaving
             while(tempNode.parentId !== -1) {
-                elementNodes[tempNode.parentId].lastSelectedChildId = tempNode.id;
-                tempNode = elementNodes[tempNode.parentId];
+                elementNodes.nodes[tempNode.parentId].lastSelectedChildId = tempNode.id;
+                tempNode = elementNodes.nodes[tempNode.parentId];
             }
-            //this.lastSelectedChildId = currentNode.id;
+            tempNode = elementNodes.nodes[goToId];
+            // then we go down a target branch selecting either previously recorded last selected child
+            // either first child, to determine the target leaf
+            while(tempNode.childrenNodesIds.length !== 0) {
+                if (tempNode.lastSelectedChildId !== -1) {
+                    tempNode = elementNodes.nodes[tempNode.lastSelectedChildId];
+                }
+                else tempNode = elementNodes.nodes[tempNode.childrenNodesIds[0]];
+            }
+            goToId = tempNode.id;
             elements[currentNode.id].classList.remove("selected");
-            // if we move to a Single node (leaf)
-            if (elementNodes[goToId].childrenNodesIds.length === 0){
-                currentNode = elementNodes[goToId];
-                elements[currentNode.id].classList.add("selected");
-            } else {
-                // if there is a lastSelectedChild - move to it, otherwise go to 1st child
-                if (elementNodes[goToId].lastSelectedChildId !== -1)
-                currentNode.goToNode(elementNodes[goToId].lastSelectedChildId);
-                else currentNode.goToNode(elementNodes[goToId].childrenNodesIds[0]);
-            }
+            currentNode = elementNodes.nodes[goToId];
+            elements[currentNode.id].classList.add("selected");
         }
     }
 }
 
 // Key Press Handler
 document.onkeydown = function onKeyDown(ev) {
-    if (currentNode === undefined && elementNodes.length !== 0
+    if (currentNode === undefined && elementNodes.nodes.length !== 0
         && ev.keyCode >= 37 && ev.keyCode <= 40){
-        currentNode = elementNodes[0];
+        currentNode = elementNodes.nodes[0];
         elements[currentNode.id].classList.add("selected");
     } else {
         switch (ev.keyCode) {
             case 38:    //top
-                currentNode.goToNode(currentNode.findNextNodeId(0));
+                currentNode.goToNode(currentNode.nodeTop);
                 break;
             case 39:    //right
-                currentNode.goToNode(currentNode.findNextNodeId(1));
+                currentNode.goToNode(currentNode.nodeRight);
                 break;
             case 40:    //bottom
-                currentNode.goToNode(currentNode.findNextNodeId(2));
+                currentNode.goToNode(currentNode.nodeBottom);
                 break;
             case 37:    //left
-                currentNode.goToNode(currentNode.findNextNodeId(3))
+                currentNode.goToNode(currentNode.nodeLeft)
                 break;
         }
     }
-    console.log(currentNode);
 }
